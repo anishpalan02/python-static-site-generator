fb1257228b7b08596ef38bb52d5d581c61f89927
anishpalan02 <104988057+anishpalan02@users.noreply.github.com>
2022-11-02 17:19:06 +0530

    'Completed'

---
+++ ssg/site.py
@@ -0,0 +1,19 @@
+from os import mkdir
+from pathlib import  Path
+import pathlib
+
+class Site():
+
+    def __init__(self,source,dest):
+        self.source=Path(source)
+        self.dest=Path(dest)
+
+    def create_dir(self,path):
+        directory=self.dest+'/'+Path.relative_to(self.source)
+        mkdir(parents=True,exist_ok=True)
+
+    def build(self):
+        mkdir(self.dist,parents=True,exist_ok=True)
+        for i in self.source.rglob('*'):
+            pass
+            
