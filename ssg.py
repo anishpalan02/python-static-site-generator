a599e145e1a5dc28ccd1c9a2ad635a2478505462
anishpalan02 <104988057+anishpalan02@users.noreply.github.com>
2022-11-03 19:18:29 +0530

    'CompletedMod4'

--- ssg.py
+++ ssg.py
@@ -4,6 +4,6 @@ import ssg.parsers
 
 
 def main(source='content',dest='dist'):
-    config={"source":source,"dest":dest,'parsers':[ssg.parsers.ResourceParser()]}
+    config={"source":source,"dest":dest,'parsers':[ssg.parsers.ResourceParser(),ssg.parsers.MarkdownParser(),ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 typer.run(main)
